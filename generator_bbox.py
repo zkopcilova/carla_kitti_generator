Alpha, rotation, minor fixes

generator_bbox.py
@@ -111,7 +111,6 @@ def p3d_to_p2d_bb(p3d_bb):
     p2d_bb = np.array([min_x,min_y,max_x,max_y])
     return p2d_bb
 
-
 ### Get numpy 2D array of vehicles' location and rotation from world reference, also locations from sensor reference
 def get_list_transform(vehicles_list, sensor):
     t_list = []
@@ -174,3 +173,13 @@ def calculate_bbox_occlusion(bbox, depth_image):
         if point_is_occluded(vertex[0,0], vertex[0,1], vertex[0,2], depth_map):
             occluded_vertices += 1
     return float(occluded_vertices/8)
+
+def get_observation_angle(vehicle, sensor):
+    v = vehicle.get_transform()
+    transform = np.array([v.location.x , v.location.y , v.location.z, 1])
+    sensor_world_matrix = get_matrix(sensor.get_transform())
+    world_sensor_matrix = np.linalg.inv(sensor_world_matrix)
+    transform_s = np.dot(world_sensor_matrix, transform.T).T
+    v_angle = np.arctan2(transform_s[1,0],transform_s[0,0]) * 180 / np.pi
+    v_angle = deg_to_rad(v_angle)
+    return v_angle

generator_utils.py
@@ -32,9 +32,9 @@ TRUCKS = ['vehicle.carlamotors.carlacola', 'vehicle.carlamotors.firetruck']
 BIKE = 'vehicle.bh.crossbike'
 
 def crop_to_range(value, min, max):
-    if value < min:
+    if value <= min:
         value = min
-    elif value > max:
+    elif value >= max:
         value = max - 1
     return value
 
@@ -43,6 +43,24 @@ def calculate_area(x_min, x_max, y_min, y_max):
 
 def deg_to_rad(degrees):
     return degrees * pi / 180
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
     
 def point_is_occluded(vertex_x, vertex_y, vertex_depth, depth_map):